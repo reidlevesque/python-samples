fd333e044cbe5ad547b07a8c7ed2c5bddd15d59a
Reid Levesque <reid.levesque@gmail.com>
2018-02-19 18:03:37 -0500

    Simple class and test.

---
+++ unittest/apiwrapper.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""Class that wraps some REST API"""
+
+import requests
+
+
+class ApiWrapper(object):
+    """Class that wraps some REST API"""
+
+    def __init__(self, api_url: str):
+        self._api_url = api_url
+
+    def add_user(self, first_name: str, last_name: str) -> str:
+        """Adds a new user to the system and returns its ID"""
+        params = {'first_name': first_name,
+                  'last_name': last_name}
+        response = requests.post(self._api_url + '/users', json=params)
+        if response.status_code != 201:
+            raise RuntimeError
+
+        return response.json()['id']
+
+    def get_user(self, id: str) -> str:
+        """Retrieves a user from the system and returns its Full Name"""
+        response = requests.get(self._api_url + '/users/' + id)
+        if response.status_code != 200:
+            raise RuntimeError
+
+        return response.json()['first_name'] + ' ' + response.json()['last_name']
---
+++ unittest/apiwrapper_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""Unit tests for ApiWrapper"""
+
+import unittest
+import requests_mock
+
+from apiwrapper import ApiWrapper
+
+API_URL = 'http://example.com'
+
+
+@requests_mock.mock()
+class ApiWrapperTest(unittest.TestCase):
+    """Tests ApiWrapper"""
+
+    def test_add_user(self, mock):
+        wrapper = ApiWrapper(API_URL)
+
+        mock.post(API_URL + '/users', text='Unauthorized', status_code=401)
+        with self.assertRaises(RuntimeError):
+            wrapper.add_user('The', 'User')
+
+        mock.post(API_URL + '/users', text='{"id": "1234"}', status_code=201)
+        self.assertEqual('1234', wrapper.add_user('The', 'User'))
+
+    def test_get_user(self, mock):
+        wrapper = ApiWrapper(API_URL)
+
+        mock.get(API_URL + '/users/1234', text='Unauthorized', status_code=401)
+        with self.assertRaises(RuntimeError):
+            wrapper.get_user('1234')
+
+        mock.get(API_URL + '/users/1234',
+                 text='{"first_name": "The", "last_name": "User"}', status_code=200)
+        self.assertEqual('The User', wrapper.get_user('1234'))
+
+
+if __name__ == '__main__':
+    unittest.main()
